5493725d6bc740bf0b0a3baf1067751c6ea81e3d
eileenplietker <eileenplietker@web.de>
2023-07-19 15:59:13 +0200

    Body
    Habe einen Body erstellt, damit die Struktur deutlicher wird (oben / unten)

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,20 @@
 import './App.css';
 import SearchBar from './components/SearchBar';
 import Sidebar from "./components/Sidebar";
+import React from 'react';
+import Body from './components/body';
+import Footer from './components/footer';
 
 function App() {
   return (
-    <div className="container">
+    <div className='App'>
       <SearchBar/>
       <Sidebar/>
+      <Body/>
+      <Footer/>
     </div>
   );
 }
 
 export default App;
+
