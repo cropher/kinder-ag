1adb3125e1649199f693d738d30ce6948e404df4
eileenplietker <eileenplietker@web.de>
2023-07-21 14:09:06 +0200

    Footer vorläufig fertig
    Soweit erstmal fertig. Noch keine Links hinterlegt. Logo fehlt noch.

--- client/src/components/footer.jsx
+++ client/src/components/footer.jsx
@@ -2,31 +2,45 @@
 
 import React from 'react';
 import './footer.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+    faFacebook,
+    faInstagram
+  } from "@fortawesome/free-brands-svg-icons";
 
 const Footer=()=>{
     return (
         <div className='footer'>
-            <p>Hier kommt links Logo und rechts Social Media</p>
+            <div className='footer-above'>
+            <p>Hier kommt links Logo</p>
+                <div class="social-container">
+                    <a href="/"
+                        className="social">
+                        <FontAwesomeIcon icon={faInstagram} size="2x" />
+                    </a>
+                    <a href="/"
+                        className="social">
+                        <FontAwesomeIcon icon={faFacebook} size="2x" />
+                    </a>
+            </div>
+        </div>
         
         <hr></hr>
         
             <div className='footer-below'>
-                <ul className='footer-below-list'>
-                    <li>
-                        <p className='footer-copyright'>
+                <div className='footer-copyright'>
                             <p>
                              @{new Date().getFullYear()} FrüKiBi. All rights reserved.
                              </p>
-                         </p>
-                    </li>
-                        <p className='footer-below-links'>
+                </div>
+                <div className='footer-below-links'>
                              <a href='/terms'><div><p>Terms & Conditions</p></div></a>
                              <a href='/impressum'><div><p>Impressum</p></div></a>
                             <a href='/datenschutz'><div><p>Datenschutzerklärung</p></div></a>
-                        </p>
-                </ul>
-            </div>  
+                </div>
+            </div>
         </div>
+
     )
 }
 
