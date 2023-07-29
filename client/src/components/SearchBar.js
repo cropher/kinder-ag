84eda541175f7cfb21385f54d5597bdb6547e80c
cropher <christopher.burgholz@gmail.com>
2023-07-30 00:08:51 +0200

    Axios Code hinzugefügt
    Axios Code hinzugefügt

--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -4,13 +4,20 @@ import React, {useState} from 'react'
 import searchIcon from "../img/searchIcon.svg"
 
 
-const SearchBar = () => {
+const SearchBar = ({getQuery}) => {
 
+    // der usestate hook wird definiert
     const [text, setText] = useState("")
+
+    // die folgende Funktion nimmt den value der Texteingabe (q) aus der Funktion unten und gibt ihn an die setText Funktion
+    // damit wird variable "text" aktualisiert
     const onChange = (q)=>{
         setText(q)
     }
 
+    // hier wird die SearchBar definiert
+    // sie enthält ein onChange event. Wenn in die SearchBar text eingegeben wird, löst dies die obige Funktion aus.
+    // der value der Texteingabe (e.target.value) wird dabei als prop an die obige Funktion gegeben.
     return (
         <section className="searchbar">
             <form>   
