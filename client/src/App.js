b7d19bbc7f4075d02958472c39c2b8fe16bcdc1e
cropher <christopher.burgholz@gmail.com>
2023-07-30 00:07:44 +0200

    Axios installiert, importiert und angefangen Code für Datenbankabfrage hinzuzufügen

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import SearchBar from './components/SearchBar';
 import Sidebar from "./components/Sidebar";
 // import Article from "./components/Article";
@@ -7,6 +8,18 @@ import Footer from "./components/footer.jsx";
 
 
 function App() {
+
+  // der folgende useState Hook wird benutzt, um das query durch die Texteingabe zu updaten
+  // das query wird in der URL verwendet, mit der die Daten aus der API abgerufen werden
+  const [query, setQuery] = useState("")
+
+  // wir nutzen einen useEffect Hook, um die component mit der API zu synchronisieren
+
+  useEffect (()=>{
+    const fetchItems
+
+  })
+
   return (
     <div className='App'>
       <body>
