d499c7993eaaedbc72d8e6eb46794ea3303298b9
TillBongers <131911424+TillBongers@users.noreply.github.com>
2023-08-05 21:51:32 +0200

    text search and competence search
    everything working but the location=indoor/outdoor

--- server/app.js
+++ server/app.js
@@ -9,6 +9,11 @@ const app = express();
 // ------------------------------------------------------------------------------------
 // routes:
 
+// welcome
+app.get('/', (req, res) => {
+    res.json({mssg: 'Welcome to the API'});
+});
+
 // get all articles
 app.get('/articles', async (req, res) => {
   try {
@@ -16,7 +21,7 @@ app.get('/articles', async (req, res) => {
     const collection = await connectToDatabase();
 
     // Fetch all documents from the collection
-    const documents = await collection.find().toArray();
+    const documents = await collection.find({}).toArray();
 
     // Send the documents as the response in JSON format
     res.json(documents);
@@ -32,15 +37,33 @@ app.get('/articles/filter', async (req, res) => {
     const collection = await connectToDatabase();
 
     // Extract query parameters for filtering (e.g., /articles/filter?author=Max%20Meier&age=5)
-    const { author, age } = req.query;
+    const queryParams = req.query;
 
     // Build the filter object based on the query parameters
     const filter = {};
-    if (author) {
-      filter.author = author;
+       
+    // funktioniert noch nicht
+    if (queryParams.location) {
+
+        const locationArray = queryParams.competence.split(',');
+
+        filter.location = { $in: locationArray };
     }
-    if (age) {
-      filter.age = parseInt(age);
+
+    // possible age array values in database must be matched exactly -> eighter "under_3" or "over_3"
+    if (queryParams.age) {
+        
+        filter.age = queryParams.age;
+    }
+
+    // Frontend-Schema: Multiple competences must be separated by a comma (http-encoding: '%2C') 
+    // e.g. "localhost:5000/articles/filter?competence=music%2Csocial"
+    if (queryParams.competence) {
+        // convert comma-seperated competence options in the http string to an array
+        const competenceArray = queryParams.competence.split(',');
+        
+        // add all options in the array to the competence key 
+        filter.competence = { $in: competenceArray };
     }
 
     // Fetch documents from the collection based on the filter
@@ -54,6 +77,35 @@ app.get('/articles/filter', async (req, res) => {
   }
 });
 
+// Full Text Search Route
+// e.g. "localhost:5000/articles/search?search=tanz"
+app.get('/articles/search', async (req, res) => {
+  try {
+    // Get the collection by calling the connectToDatabase function
+    const collection = await connectToDatabase();
+
+    // Extract the search query parameter
+    const { search } = req.query;
+
+    // Build the filter object for the title search
+    const filter = {
+      $or: [
+        { title: { $regex: search, $options: 'i' } },
+        { body: { $regex: search, $options: 'i' } },
+        { author: { $regex: search, $options: 'i' } }
+      ]
+    };
+
+    // Fetch documents from the collection based on the title search filter
+    const documents = await collection.find(filter).toArray();
+
+    // Send the search results as the response in JSON format
+    res.json(documents);
+  } catch (error) {
+    console.error('Error searching for documents:', error);
+    res.status(500).json({ error: 'Could not perform the search' });
+  }
+});
 
 // ---------------------------------------------------------------------------------
 // express app
