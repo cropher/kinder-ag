b366746da7b215c8dda3bdd9f7566f9e243e95ef
cropher <christopher.burgholz@gmail.com>
2023-08-14 20:57:32 +0200

    Card.js und CSS angepsst
    Die Cards angepasst, sodass der Titel der Aktivität korrekt abgerufen wird. Die anderen Items aus der Card rausgenommen, damit nur noch der Titel angezeigt wird. Das dazugehörige CSS geschrieben.

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -1,19 +1,4 @@
 // Karte, die durch Anklicken den dazugehörigen Artikel aufruft.
-// import React from 'react'
-// import CharacterItem from './CharacterItem'
-// import Spinner from '../ui/Spinner'
-
-// const CharacterGrid = ({ items, isLoading }) => {
-//   return isLoading ? (
-//     <Spinner />
-//   ) : (
-//     <section className='cards'>
-//       {items.map((item) => (
-//         <CharacterItem key={item.char_id} item={item}></CharacterItem>
-//       ))}
-//     </section>
-//   )
-// }
 
 import React from 'react'
 
@@ -25,20 +10,10 @@ const CharacterItem = ({ item }) => {
           <img src={item.img} alt='' />
         </div>
         <div className='card-back'>
-          <h1>{item.name}</h1>
-          <ul>
-            <li>
-              <strong>Aktivität:</strong> {item.activity}
-            </li>
-            <li>
-              <strong>Altersgruppe:</strong> {item.age}
-            </li>
-            <li>
-              <strong>Kategorie:</strong> {item.competence}
-            </li>
-            <li>
-              <strong>Status:</strong> {item.status}
-            </li>
+          <h1>{item.title}</h1>
+          <ul className="Items">
+
+
           </ul>
         </div>
       </div>
