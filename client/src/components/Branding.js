be739ba1741eeda6a1e48b4d3f2b9c78ea37f987
cropher <christopher.burgholz@gmail.com>
2023-08-19 13:17:37 +0200

    CSS geändert und Logo hinzugefügt

---
+++ client/src/components/Branding.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import FrüKiBi_Logo_farbig from "../img/FrüKiBi_Logo_farbig.svg"
+
+const Branding = () => {
+  return (
+    <div>
+        <img src={FrüKiBi_Logo_farbig}/>
+    </div>
+  )
+}
+
+export default Branding
