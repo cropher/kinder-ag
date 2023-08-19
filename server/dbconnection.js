8c8eedafcc04eb27d15234e9639bbb0b3385f56f
cropher <christopher.burgholz@gmail.com>
2023-08-19 13:15:45 +0200

    Revert "Backend with recycled connection"

--- server/dbconnection.js
+++ server/dbconnection.js
@@ -4,8 +4,6 @@
 
 const { MongoClient } = require('mongodb');
 
-let dbConnection;
-
 // Replace the placeholder with your actual MongoDB Atlas connection string
 const MONGODB_URI = 'mongodb+srv://claudeGable:DbRWfUUkl0kcUj15@cluster48153.wnmcmz8.mongodb.net/?retryWrites=true&w=majority';
 
@@ -15,23 +13,17 @@ const DB_NAME = 'frükibi';
 // Function to establish the database connection and return the collection
 async function connectToDatabase() {
   try {
-    if(dbConnection){
-      return dbConnection;
-    }
-    else{
-      // Connect to the MongoDB Atlas cluster
-      const client = await MongoClient.connect(MONGODB_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      });
+    // Connect to the MongoDB Atlas cluster
+    const client = await MongoClient.connect(MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
 
-      // Access the specified database
-      dbConnection = client.db(DB_NAME);
+    // Access the specified database
+    const db = client.db(DB_NAME);
 
-      // Access the specified collection and return it
-      return dbConnection;
-    }
-    
+    // Access the specified collection and return it
+    return db.collection('articles');
   } catch (error) {
     console.error('Error connecting to the database:', error);
     throw error;
